a9d75b9167c66d555a170d009eecf252b2ae7dfd
Jamie <jamielthompson29.jt@gmail.com>
2017-05-04 22:56:05 -0500

    completed the rest of the tutorial. may address extra stuff mentioned later

--- react/tictactoe_tutorial/tictactoe_tutorial/src/index.js
+++ react/tictactoe_tutorial/tictactoe_tutorial/src/index.js
@@ -46,7 +46,7 @@ class Board extends React.Component {
 
 class Game extends React.Component {
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -57,6 +57,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
@@ -67,13 +68,31 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext:true,
         };
     }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) ? false : true,
+        });
+    }
+
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Move #' + move : 'Game start';
+            return (
+                <li key={move}>
+                    <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+                </li>
+            );
+        });
 
         let status;
         if (winner) {
@@ -91,7 +110,7 @@ class Game extends React.Component {
                 </div>
             <div className="game-info">
                 <div>{status}</div>
-                <ol>{/* TODO */}</ol>
+                <ol>{moves}</ol>
                 </div>
             </div>
         );
